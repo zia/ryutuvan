65a9d3fe110e8ea83b83710bde3e83aeb5a26a27
zia <gm.bellalhossain@gmail.com>
2017-08-24 18:36:39 +0600

    #Local Commit # Be careful

--- application/config/config.php
+++ application/config/config.php
@@ -36,8 +36,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /*
 * Dynamic
 */
-// $root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
-$root = "https://".$_SERVER['HTTP_HOST'];
+$root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
+// $root = "https://".$_SERVER['HTTP_HOST'];
 $root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $root;
 
@@ -470,7 +470,9 @@ $config['csrf_token_name'] = 'EciJDgK1SZ';
 $config['csrf_cookie_name'] = 'BizJ4ZAJYH';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = FALSE;
-$config['csrf_exclude_uris'] = array();
+$config['csrf_exclude_uris'] = array(
+		'search/update'
+	);
 
 /*
 |--------------------------------------------------------------------------
