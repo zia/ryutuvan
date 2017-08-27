beca3e8d28bbf3136acbfd2ac7eae1850e7ac6b7
Ziaur Rahman <zia.1993.rahman@gmail.com>
2017-08-27 18:29:12 +0600

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -36,8 +36,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /*
 * Dynamic
 */
-$root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
-// $root = "https://".$_SERVER['HTTP_HOST'];
+// $root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
+$root = "https://".$_SERVER['HTTP_HOST'];
 $root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $root;
 
