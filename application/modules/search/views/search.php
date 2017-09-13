2da4e75b004bff1d94d94c651f8967aaf0a7c5be
zia <gm.bellalhossain@gmail.com>
2017-09-13 18:32:38 +0600

    #Scrolling Gone

--- application/modules/search/views/search.php
+++ application/modules/search/views/search.php
@@ -5,6 +5,7 @@
 <img src="<?=base_url('assets/img/loader.gif')?>" alt="Searching.." id="loader">
 
 <!-- Mother Table -->
+<div class="scroll_div">
 <table id="mother_table">
 <!-- Header table begins -->
 	<tr>
@@ -78,186 +79,184 @@
 		<!-- If kbd navigation is required, should be applied on this table -->
 		<!-- Calculation -->
 		<?=form_open('search/search_result')?>
-		<div class="scroll_div">
-		<table align="center" cellspacing="1" class="SO_bg" _fixedhead="rows:3; cols:5">
-		<!--Data Table Header Starts -->
-		<!-- Row 1 : Main Headers -->
-		<tr>
-		<!-- First 3 Headings -->
-		<th class="SO_title2" rowspan="2" nowrap>商品名</th>
-		<th class="SO_title3" rowspan="2" nowrap></th>
-		<th class="SO_title4" colspan="3">全体</th>
+			<table align="center" cellspacing="1" class="SO_bg" _fixedhead="rows:0; cols:5">
+			<!--Data Table Header Starts -->
+			<!-- Row 1 : Main Headers -->
+			<tr>
+			<!-- First 3 Headings -->
+			<th class="SO_title2" rowspan="2" nowrap>商品名</th>
+			<th class="SO_title3" rowspan="2" nowrap></th>
+			<th class="SO_title4" colspan="3">全体</th>
 
-		<!-- Border -->
-		<th class="SO_th3" nowrap></th>
+			<!-- Border -->
+			<th class="SO_th3" nowrap></th>
 
-		<?php
-		/**
-		* This is dynamic header rows
-		*/
-		$j=0;
-		foreach ($headings as $heading) {
-		?>
-		<!-- Dynamic column header name and number here -->
-		<th class="SO_title3 dynamic_header" colspan="3">
-		<?=$heading->title?>
-		</th>
-		<?php		
-		$j++;
-		}
-		?>
-		</tr>
-		<!-- Row 2 : Sub Headers-->
-		<tr>
-		<?php $nm = 1; foreach($subheadings as $subheading) { ?>
-		<th class="SO_title5 static_sub_header" id="ssh_<?=$nm?>" nowrap>
-		<?=$subheading->title?>
-		</th>
-		<?php $nm++; } ?>
+			<?php
+			/**
+			* This is dynamic header rows
+			*/
+			$j=0;
+			foreach ($headings as $heading) {
+			?>
+			<!-- Dynamic column header name and number here -->
+			<th class="SO_title3 dynamic_header" colspan="3">
+			<?=$heading->title?>
+			</th>
+			<?php		
+			$j++;
+			}
+			?>
+			</tr>
+			<!-- Row 2 : Sub Headers-->
+			<tr>
+			<?php $nm = 1; foreach($subheadings as $subheading) { ?>
+			<th class="SO_title5 static_sub_header" id="ssh_<?=$nm?>" nowrap>
+			<?=$subheading->title?>
+			</th>
+			<?php $nm++; } ?>
 
-		<!-- Border -->
-		<th class="SO_th3" nowrap></th>
+			<!-- Border -->
+			<th class="SO_th3" nowrap></th>
 
-		<?php
-		/**
-		* Each Headers got 3 sub heading
-		*/
-		foreach($headings as $heading) {
-		foreach ($subheadings as $subheading) {
-		?>
-		<th class="SO_title5" nowrap><?=$subheading->title?></th>
+			<?php
+			/**
+			* Each Headers got 3 sub heading
+			*/
+			foreach($headings as $heading) {
+			foreach ($subheadings as $subheading) {
+			?>
+			<th class="SO_title5" nowrap><?=$subheading->title?></th>
 
-		<?php		
-		}
-		}
-		?>
-		</tr>
-		<!-- Row 3 : Search and Sort -->
-		<tr id="search_row">
-		<!-- There is Five Fixed header Columns -->
-		<th colspan="5"></th>
-		<!-- Centering the search field -->
-		<th colspan="<?=count($headings) * 3 + 1?>">&nbsp;</th>
+			<?php		
+			}
+			}
+			?>
+			</tr>
+			<!-- Row 3 : Search and Sort -->
+			<tr id="search_row">
+			<!-- There is Five Fixed header Columns -->
+			<th colspan="5"></th>
+			<!-- Centering the search field -->
+			<th colspan="<?=count($headings) * 3 + 1?>">&nbsp;</th>
 
-		<!-- Search Field -->
-		<!--<th colspan="9">
-		</th>-->
+			<!-- Search Field -->
+			<!--<th colspan="9">
+			</th>-->
 
-		<!-- This empty cells colspan needs to calculated -->
-		<!--<th colspan="8">&nbsp;</th>-->
-		</tr>
-		<!--Data Table Header Ends -->
+			<!-- This empty cells colspan needs to calculated -->
+			<!--<th colspan="8">&nbsp;</th>-->
+			</tr>
+			<!--Data Table Header Ends -->
 
-		<!--Data Table Data Starts -->
+			<!--Data Table Data Starts -->
 
-		<?php
-		/**
-		* Data Rows
-		*/
-		$r = 0;
-		$p = 0;
-		foreach ($products as $product) {
-		?>
-		<!-- Row <?=$r?> white -->
-		<tr>
-		<td class='SO_td1' rowspan='2'>
-		<table border='0' width='100%' class="<?='table_'.$product->row?>">
-		<tr>
-		<td class="lefty" style="padding: 5px;" nowrap>
-		<?=$product->title?>
-		</td>
-		</tr>
-		<tr>
-		<td class="righty">
-		<?=$product->quantity?>
-		</td>
-		</tr>
-		</table>
-		</td>
-		<td class='SO_td1' style="padding-top: 12px;" nowrap>発注数</td>
+			<?php
+			/**
+			* Data Rows
+			*/
+			$r = 0;
+			$p = 0;
+			foreach ($products as $product) {
+			?>
+			<!-- Row <?=$r?> white -->
+			<tr>
+			<td class='SO_td1' rowspan='2'>
+			<table border='0' width='100%' class="<?='table_'.$product->row?>">
+			<tr>
+			<td class="lefty" style="padding: 5px;" nowrap>
+			<?=$product->title?>
+			</td>
+			</tr>
+			<tr>
+			<td class="righty">
+			<?=$product->quantity?>
+			</td>
+			</tr>
+			</table>
+			</td>
+			<td class='SO_td1' style="padding-top: 12px;" nowrap>発注数</td>
 
-		<!-- Summation Columns : Row 1 starts -->
-		<!--
-		ca = column a
-		cb = column b
-		cc = column c
-		-->
-		<td class='SO_tdtr1c0' id="r<?=$r?>ca">0</td>
-		<td class='SO_tdtr1c1' id="r<?=$r?>cb">0</td>
-		<td class='SO_tdtr1c2' id="r<?=$r?>cc">0</td>
-		<!-- Summation Columns : Row 1 ends -->
+			<!-- Summation Columns : Row 1 starts -->
+			<!--
+			ca = column a
+			cb = column b
+			cc = column c
+			-->
+			<td class='SO_tdtr1c0' id="r<?=$r?>ca">0</td>
+			<td class='SO_tdtr1c1' id="r<?=$r?>cb">0</td>
+			<td class='SO_tdtr1c2' id="r<?=$r?>cc">0</td>
+			<!-- Summation Columns : Row 1 ends -->
 
-		<!-- Border -->
-		<th class='SO_th3'></th>
+			<!-- Border -->
+			<th class='SO_th3'></th>
 
-		<?php
-		$c=0;
-		foreach ($headings as $heading) {
-		foreach ($subheadings as $subheading) {
-		?>
-		<td class='SO_td1'>0</td>
-		<?php
-		}
-		}
-		?>
-		</tr>
-		<?php
-		$r++;
-		?>
+			<?php
+			$c=0;
+			foreach ($headings as $heading) {
+			foreach ($subheadings as $subheading) {
+			?>
+			<td class='SO_td1'>0</td>
+			<?php
+			}
+			}
+			?>
+			</tr>
+			<?php
+			$r++;
+			?>
 
-		<!-- Row <?=$r?> red -->
-		<tr>
-		<td class='SO_tdn'>確定</td>
+			<!-- Row <?=$r?> red -->
+			<tr>
+			<td class='SO_tdn'>確定</td>
 
-		<!-- Summation Columns : Row 2 -->
-		<!--
-		ca = column a
-		cb = column b
-		cc = column c
-		-->
-		<td class='SO_tdtr2c0' id="r<?=$r?>ca">
-		<?=$product->total_0?>
-		</td>
-		<td class='SO_tdtr2c1' id="r<?=$r?>cb">
-		<?=$product->total_1?>
-		</td>
-		<td class='SO_tdtr2c2' id="r<?=$r?>cc">
-		<?=$product->total_2?>
-		</td>
+			<!-- Summation Columns : Row 2 -->
+			<!--
+			ca = column a
+			cb = column b
+			cc = column c
+			-->
+			<td class='SO_tdtr2c0' id="r<?=$r?>ca">
+			<?=$product->total_0?>
+			</td>
+			<td class='SO_tdtr2c1' id="r<?=$r?>cb">
+			<?=$product->total_1?>
+			</td>
+			<td class='SO_tdtr2c2' id="r<?=$r?>cc">
+			<?=$product->total_2?>
+			</td>
 
-		<!-- Border -->
-		<th class='SO_th3'></th>
+			<!-- Border -->
+			<th class='SO_th3'></th>
 
-		<?php
-		$c = 0;
-		foreach ($headings as $heading) {
-		foreach ($subheadings as $subheading) {
-		?>
-		<td class='SO_td4'>
-		<input
-		class="SO_input2"
-		type="text"
-		pattern="[0-9].{0,}"
-		name="r<?=$r?>c<?=$c?>"
-		data-id="<?=$product->id?>"
-		<?=$infos[$p]->data>10000 ? 'title="'.$infos[$p]->data.'"' : ''?>
-		value="<?=$infos[$p++]->data?>"
-		id="r<?=$r?>c<?=$c++?>"
-		maxlength="8"
-		minlength="1"
-		>
-		</td>
-		<?php
-		}
-		}
-		?>
-		</tr>
-		<?php
-		$r++;
-		}
-		?>
-		</table>
-		</div>
+			<?php
+			$c = 0;
+			foreach ($headings as $heading) {
+			foreach ($subheadings as $subheading) {
+			?>
+			<td class='SO_td4'>
+			<input
+			class="SO_input2"
+			type="text"
+			pattern="[0-9].{0,}"
+			name="r<?=$r?>c<?=$c?>"
+			data-id="<?=$product->id?>"
+			<?=$infos[$p]->data>10000 ? 'title="'.$infos[$p]->data.'"' : ''?>
+			value="<?=$infos[$p++]->data?>"
+			id="r<?=$r?>c<?=$c++?>"
+			maxlength="8"
+			minlength="1"
+			>
+			</td>
+			<?php
+			}
+			}
+			?>
+			</tr>
+			<?php
+			$r++;
+			}
+			?>
+			</table>
 		<?=form_close()?>
 		</td>
 		</tr>
@@ -266,3 +265,4 @@
 		<!-- data tables ends -->
 	</tr>
 </table>
+</div>
--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -10,7 +10,7 @@
 	<head>
 		<!-- Meta -->
 		<!--<meta http-equiv="refresh" content="60">-->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta http-equiv="Content-Script-Type" content="text/javascript; charset=UTF-8">
 		<!-- Title -->
